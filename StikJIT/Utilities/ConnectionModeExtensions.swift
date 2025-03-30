72d2863f693d01afc76109205a789e300f18877b
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-30 16:48:38 -0500

    Update ConnectionModeExtensions.swift

--- StikJIT/Utilities/ConnectionModeExtensions.swift
+++ StikJIT/Utilities/ConnectionModeExtensions.swift
@@ -6,14 +6,7 @@
 
 import Foundation
 
-// This enum mirrors the Objective-C ConnectionMode enum defined in heartbeat.h
-// to provide type-safe handling in Swift
-enum ConnectionMode: Int {
-    case USB = 0
-    case TCP = 1
-}
-
-// Extension to provide conversion to Objective-C ConnectionMode
+// Instead of redefining ConnectionMode, just extend the existing one
 extension ConnectionMode {
     // Convert Swift ConnectionMode to Objective-C ConnectionMode
     var toObjC: __ObjC.ConnectionMode {
@@ -23,11 +16,6 @@ extension ConnectionMode {
         case .TCP:
             return .TCP
         }
-    }
-    
-    // Create Swift ConnectionMode from Int (useful for @AppStorage)
-    static func fromInt(_ value: Int) -> ConnectionMode {
-        return value == 0 ? .USB : .TCP
     }
 }
 
