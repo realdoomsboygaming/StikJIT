6f8cc79404ee77dba42895dd71bb4be5554c3d1a
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-30 17:37:37 -0500

    Update SettingsView.swift

--- StikJIT/Views/SettingsView.swift
+++ StikJIT/Views/SettingsView.swift
@@ -830,7 +830,7 @@ struct SettingsView: View {
         }
         .onAppear {
             loadCustomBackgroundColor()
-            let swiftMode: JITEnableContextSwiftBridge.ConnectionModeSwift = connectionMode == 0 ? .USB : .TCP
+            let swiftMode: ConnectionModeSwift = connectionMode == 0 ? .USB : .TCP
             JITEnableContext.shared().setConnectionModeSwift(swiftMode)
         }
     }
