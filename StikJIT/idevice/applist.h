746cf50e0fe7337357b03f2a4384cb8d6f0d941c
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-30 15:38:36 -0500

    Update applist.h

--- StikJIT/idevice/applist.h
+++ StikJIT/idevice/applist.h
@@ -11,4 +11,7 @@
 
 NSDictionary<NSString*, NSString*>* list_installed_apps(TcpProviderHandle* provider, NSString** error);
 
+// Add function for USB app listing
+NSDictionary<NSString*, NSString*>* list_installed_apps_usb(UsbmuxdAddrHandle* addr, NSString** error);
+
 #endif /* APPLIST_H */
