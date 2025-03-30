70d7e18256f91f74ccd422f6c3415fbe069b803f
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-30 18:28:29 -0500

    Update applist.m

--- StikJIT/idevice/applist.m
+++ StikJIT/idevice/applist.m
@@ -14,13 +14,12 @@
 
 #include "applist.h"
 
-// New function to list apps over USB with less restrictive filtering
+// New function to list apps over USB with simpler filtering
 NSDictionary<NSString*, NSString*>* list_installed_apps_usb(UsbmuxdAddrHandle* addr, NSString** error) {
     IdeviceErrorCode err = IdeviceSuccess;
     
     // Create USB provider
     UsbmuxdProviderHandle *provider = NULL;
-    // Using 0 for device_id when UDID is empty string to avoid any issues
     err = usbmuxd_provider_new(addr, 1, "", 0, "StikJIT", &provider);
     if (err != IdeviceSuccess) {
         *error = @"Failed to create USB provider";
@@ -39,7 +38,7 @@ NSDictionary<NSString*, NSString*>* list_installed_apps_usb(UsbmuxdAddrHandle* a
     void *apps = NULL;
     size_t apps_len = 0;
     
-    // Get User applications instead of "Any" to focus on user apps
+    // Get all User applications
     err = installation_proxy_get_apps(client, "User", NULL, 0, &apps, &apps_len);
     if (err != IdeviceSuccess) {
         installation_proxy_client_free(client);
@@ -51,12 +50,11 @@ NSDictionary<NSString*, NSString*>* list_installed_apps_usb(UsbmuxdAddrHandle* a
     plist_t *app_list = (plist_t *)apps;
     
     NSMutableDictionary<NSString*, NSString*>* ans = [[NSMutableDictionary alloc] init];
-    NSMutableDictionary<NSString*, NSString*>* fallbackApps = [[NSMutableDictionary alloc] init];
     
     for (size_t i = 0; i < apps_len; i++) {
         plist_t app = app_list[i];
         
-        // Get the bundle identifier regardless of entitlements
+        // Get the bundle identifier
         plist_t bundle_id_node = plist_dict_get_item(app, "CFBundleIdentifier");
         if (bundle_id_node) {
             char *bundle_id = NULL;
@@ -80,37 +78,17 @@ NSDictionary<NSString*, NSString*>* list_installed_apps_usb(UsbmuxdAddrHandle* a
                     plist_get_string_val(display_name_node, &app_name);
                 }
                 
-                // If still no name, use a default
+                // If still no name, use the bundle ID
                 if (app_name == NULL) {
-                    app_name = strdup("Unknown");
+                    app_name = strdup(bundle_id);
                 }
             }
 
             NSString *bundleIDStr = [NSString stringWithCString:bundle_id encoding:NSASCIIStringEncoding];
             NSString *appNameStr = [NSString stringWithCString:app_name encoding:NSASCIIStringEncoding];
             
-            // Now check for entitlements - prioritize apps with get-task-allow
-            plist_t entitlements = plist_dict_get_item(app, "Entitlements");
-            if (entitlements) {
-                plist_t taskAllowNode = plist_dict_get_item(entitlements, "get-task-allow");
-                if (taskAllowNode) {
-                    uint8_t isAllowed = 0;
-                    plist_get_bool_val(taskAllowNode, &isAllowed);
-                    if (isAllowed) {
-                        // This is a JIT-capable app, add it to our main dictionary
-                        ans[bundleIDStr] = appNameStr;
-                    } else {
-                        // Not JIT-capable, add to fallback dictionary
-                        fallbackApps[bundleIDStr] = appNameStr;
-                    }
-                } else {
-                    // No get-task-allow node, add to fallback
-                    fallbackApps[bundleIDStr] = appNameStr;
-                }
-            } else {
-                // No entitlements, add to fallback
-                fallbackApps[bundleIDStr] = appNameStr;
-            }
+            // Add all apps to the dictionary
+            ans[bundleIDStr] = appNameStr;
             
             free(bundle_id);
             free(app_name);
@@ -120,44 +98,10 @@ NSDictionary<NSString*, NSString*>* list_installed_apps_usb(UsbmuxdAddrHandle* a
     installation_proxy_client_free(client);
     usbmuxd_provider_free(provider);
     
-    // If we found JIT-capable apps, return those
-    if (ans.count > 0) {
-        return ans;
-    }
-    
-    // Otherwise, use the fallback list (all apps)
-    // Only include some common development apps in the fallback
-    NSMutableDictionary<NSString*, NSString*>* filteredFallback = [[NSMutableDictionary alloc] init];
-    NSArray* knownDevApps = @[
-        @"com.apple.TestFlight",
-        @"com.apple.Playground",
-        @"com.apple.dt.",  // Prefix for Apple dev tools
-        @"org.webkit.",    // WebKit-based apps
-        @"com.blackcats.alpha", // Unity-based apps (example)
-        @"com.unity.",     // Unity apps
-        @"com.yourcompany." // Default Xcode app prefix
-    ];
-    
-    // Filter to only include apps likely to need JIT
-    for (NSString* bundleID in fallbackApps) {
-        for (NSString* prefix in knownDevApps) {
-            if ([bundleID hasPrefix:prefix]) {
-                filteredFallback[bundleID] = fallbackApps[bundleID];
-                break;
-            }
-        }
-    }
-    
-    // If we have filtered apps, return those
-    if (filteredFallback.count > 0) {
-        return filteredFallback;
-    }
-    
-    // Last resort: return all apps
-    return fallbackApps;
+    return ans;
 }
 
-// Original function updated with same improvements
+// Original function updated with similar simpler approach
 NSDictionary<NSString*, NSString*>* list_installed_apps(TcpProviderHandle* provider, NSString** error) {
     IdeviceErrorCode err = IdeviceSuccess;
 
@@ -180,12 +124,11 @@ NSDictionary<NSString*, NSString*>* list_installed_apps(TcpProviderHandle* provi
     plist_t *app_list = (plist_t *)apps;
     
     NSMutableDictionary<NSString*, NSString*>* ans = [[NSMutableDictionary alloc] init];
-    NSMutableDictionary<NSString*, NSString*>* fallbackApps = [[NSMutableDictionary alloc] init];
     
     for (size_t i = 0; i < apps_len; i++) {
         plist_t app = app_list[i];
         
-        // Get the bundle identifier regardless of entitlements
+        // Get the bundle identifier
         plist_t bundle_id_node = plist_dict_get_item(app, "CFBundleIdentifier");
         if (bundle_id_node) {
             char *bundle_id = NULL;
@@ -209,37 +152,17 @@ NSDictionary<NSString*, NSString*>* list_installed_apps(TcpProviderHandle* provi
                     plist_get_string_val(display_name_node, &app_name);
                 }
                 
-                // If still no name, use a default
+                // If still no name, use the bundle ID
                 if (app_name == NULL) {
-                    app_name = strdup("Unknown");
+                    app_name = strdup(bundle_id);
                 }
             }
 
             NSString *bundleIDStr = [NSString stringWithCString:bundle_id encoding:NSASCIIStringEncoding];
             NSString *appNameStr = [NSString stringWithCString:app_name encoding:NSASCIIStringEncoding];
             
-            // Now check for entitlements - prioritize apps with get-task-allow
-            plist_t entitlements = plist_dict_get_item(app, "Entitlements");
-            if (entitlements) {
-                plist_t taskAllowNode = plist_dict_get_item(entitlements, "get-task-allow");
-                if (taskAllowNode) {
-                    uint8_t isAllowed = 0;
-                    plist_get_bool_val(taskAllowNode, &isAllowed);
-                    if (isAllowed) {
-                        // This is a JIT-capable app, add it to our main dictionary
-                        ans[bundleIDStr] = appNameStr;
-                    } else {
-                        // Not JIT-capable, add to fallback dictionary
-                        fallbackApps[bundleIDStr] = appNameStr;
-                    }
-                } else {
-                    // No get-task-allow node, add to fallback
-                    fallbackApps[bundleIDStr] = appNameStr;
-                }
-            } else {
-                // No entitlements, add to fallback
-                fallbackApps[bundleIDStr] = appNameStr;
-            }
+            // Add all apps to the dictionary
+            ans[bundleIDStr] = appNameStr;
             
             free(bundle_id);
             free(app_name);
@@ -248,39 +171,5 @@ NSDictionary<NSString*, NSString*>* list_installed_apps(TcpProviderHandle* provi
     
     installation_proxy_client_free(client);
     
-    // If we found JIT-capable apps, return those
-    if (ans.count > 0) {
-        return ans;
-    }
-    
-    // Otherwise, use the fallback list (all apps)
-    // Only include some common development apps in the fallback
-    NSMutableDictionary<NSString*, NSString*>* filteredFallback = [[NSMutableDictionary alloc] init];
-    NSArray* knownDevApps = @[
-        @"com.apple.TestFlight",
-        @"com.apple.Playground",
-        @"com.apple.dt.",  // Prefix for Apple dev tools
-        @"org.webkit.",    // WebKit-based apps
-        @"com.blackcats.alpha", // Unity-based apps (example)
-        @"com.unity.",     // Unity apps
-        @"com.yourcompany." // Default Xcode app prefix
-    ];
-    
-    // Filter to only include apps likely to need JIT
-    for (NSString* bundleID in fallbackApps) {
-        for (NSString* prefix in knownDevApps) {
-            if ([bundleID hasPrefix:prefix]) {
-                filteredFallback[bundleID] = fallbackApps[bundleID];
-                break;
-            }
-        }
-    }
-    
-    // If we have filtered apps, return those
-    if (filteredFallback.count > 0) {
-        return filteredFallback;
-    }
-    
-    // Last resort: return all apps
-    return fallbackApps;
+    return ans;
 }
