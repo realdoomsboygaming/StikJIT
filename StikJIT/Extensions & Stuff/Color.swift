f4a3f1675e6129253cc50ac8b03ed079ac17fc9b
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-30 16:16:16 -0500

    Update Color.swift

--- StikJIT/Extensions & Stuff/Color.swift
+++ StikJIT/Extensions & Stuff/Color.swift
@@ -9,29 +9,66 @@
 import SwiftUI
 
 extension Color {
+    static let primaryBackground = Color(UIColor.systemBackground)
+    static let secondaryText = Color(UIColor.secondaryLabel)
+    
     func toHex() -> String? {
-        let components = UIColor(self).cgColor.components
-        let r = Float(components?[0] ?? 0)
-        let g = Float(components?[1] ?? 0)
-        let b = Float(components?[2] ?? 0)
-        let hex = String(format: "%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
-        return "#" + hex
+        let uic = UIColor(self)
+        guard let components = uic.cgColor.components, components.count >= 3 else {
+            return nil
+        }
+        let r = Float(components[0])
+        let g = Float(components[1])
+        let b = Float(components[2])
+        var a = Float(1.0)
+        
+        if components.count >= 4 {
+            a = Float(components[3])
+        }
+        
+        if a != 1.0 {
+            return String(format: "#%02lX%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255), lroundf(a * 255))
+        } else {
+            return String(format: "#%02lX%02lX%02lX", lroundf(r * 255), lroundf(g * 255), lroundf(b * 255))
+        }
     }
     
     init?(hex: String) {
-        let r, g, b: CGFloat
-        let start = hex.index(hex.startIndex, offsetBy: 1)
-        let hexColor = String(hex[start...])
-
-        if hexColor.count == 6, let hexNumber = Int(hexColor, radix: 16) {
-            r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
-            g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
-            b = CGFloat(hexNumber & 0x0000ff) / 255
-            self.init(red: r, green: g, blue: b)
-            return
+        var hexString = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if hexString.hasPrefix("#") {
+            hexString.remove(at: hexString.startIndex)
         }
+        
+        if hexString.count != 6 && hexString.count != 8 {
+            return nil
+        }
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexString).scanHexInt64(&rgbValue)
+        
+        let r, g, b, a: CGFloat
+        
+        if hexString.count == 6 {
+            r = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
+            g = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
+            b = CGFloat(rgbValue & 0x0000FF) / 255.0
+            a = 1.0
+        } else {
+            r = CGFloat((rgbValue & 0xFF000000) >> 24) / 255.0
+            g = CGFloat((rgbValue & 0x00FF0000) >> 16) / 255.0
+            b = CGFloat((rgbValue & 0x0000FF00) >> 8) / 255.0
+            a = CGFloat(rgbValue & 0x000000FF) / 255.0
+        }
+        
+        self.init(red: r, green: g, blue: b, opacity: a)
+    }
+}
 
-        return nil
+// Add URL extension for document directory
+extension URL {
+    static var documentsDirectory: URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
     }
 }
 
