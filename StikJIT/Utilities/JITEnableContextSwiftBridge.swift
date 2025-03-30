18904fc728d46611bda49c911a9465eba126b92e
realdoomsboygaming <105606471+realdoomsboygaming@users.noreply.github.com>
2025-03-30 17:42:22 -0500

    Update JITEnableContextSwiftBridge.swift

--- StikJIT/Utilities/JITEnableContextSwiftBridge.swift
+++ StikJIT/Utilities/JITEnableContextSwiftBridge.swift
@@ -13,18 +13,4 @@ extension JITEnableContext {
         // Cast to Int32 to match the Objective-C method signature
         self.setConnectionMode(Int32(mode.rawValue))
     }
-    
-    // Create a completely different method that doesn't try to use getAppListWithError
-    public func getAppList() -> [String: String]? {
-        // Access the variable directly instead of using the method
-        let nsError = NSError(domain: "StikJIT", code: -1, userInfo: nil)
-        var error: NSError? = nsError
-        
-        // Call the Objective-C API directly
-        if let apps = list_installed_apps_usb(nil, &error) {
-            return apps as? [String: String]
-        }
-        
-        return [:]
-    }
 }
